9bd8849e3c5ee802ea47d1f6366d5644477fee4f
xTeKc <bcd3v@pm.me>
2021-08-03 17:16:58 -0400

    add if

--- src/main.rs
+++ src/main.rs
@@ -5,4 +5,10 @@ fn main() {
     //calls method args from this module and returns iterator
     //which is collected into Vec<String>, a Vector of String objects
     let args: Vec<String> = env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage:");
+        println!("sha1fetch: <wordlist.txt> <sha1hash>");
+        return;
+    }
 }
