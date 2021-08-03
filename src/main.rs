a5377d2479c01b2def82ab97050241e7fe268a52
xTeKc <bcd3v@pm.me>
2021-08-03 17:13:28 -0400

    add comments

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+//import env from standard library
 use std::env;
 
 fn main() {
+    //calls method args from this module and returns iterator
+    //which is collected into Vec<String>, a Vector of String objects
     let args: Vec<String> = env::args().collect();
 }
