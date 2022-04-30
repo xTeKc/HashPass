361013c8eba85fa5108fca59a95a138b2bb72556
xTeKc <bcd3v@pm.me>
2022-04-30 02:50:23 -0400

    add enum for tabs

--- src/main.rs
+++ src/main.rs
@@ -31,8 +31,14 @@ struct Data {
 pub enum Error {
     #[error("error reading the DB file: {0}")]
     ReadDBError(#[from] io::Error),
-    #[error("error parsing the DB file: {0)")]
+    #[error("error parsing the DB file: {0}")]
     ParseDBError(#[from] serde_json::Error)
+}
+
+#[derive(Clone, Copy, Debug)]
+enum TabItem {
+    Home,
+    HashedPass,
 }
 
 fn read_wordlist() -> Result<String, Error> {
