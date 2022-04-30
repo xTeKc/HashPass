18e2f208dce7e69b295dfd3556c730c50aff0d62
xTeKc <bcd3v@pm.me>
2022-04-30 02:27:57 -0400

    update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
+use std::{fs, io::Error};
 
+use serde::{Serialize, Deserialize};
+
+const DATABASE: &str = "./data/db.json";
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Data {
+    id: u32,
+    password: String,
+    hash: String,
+}
+
+fn read_wordlist() -> Result<String, Error> {
+    let read_list = fs::read_to_string("./wordlist.txt");
+    read_list
+}
 
 fn main() {
-
+   let read_it = read_wordlist()
+    .expect("no wordlist found");
+    println!("{:?}", read_it)
 }
