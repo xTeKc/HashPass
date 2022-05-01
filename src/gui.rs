e95cb3f022104f146f0c03827a7f9e71a3d5bea3
xTeKc <bcd3v@pm.me>
2022-04-30 23:49:32 -0400

    add struct for main style darkmode

--- src/gui.rs
+++ src/gui.rs
@@ -1,15 +1,7 @@
+mod colors;
+
 use iced::{button, container, rule, text_input, Color};
 
-//
-
-pub struct AppStyle {
+pub struct MainStyle {
     pub is_dark_mode: bool,
-    pub is_running: bool,
-    pub opacity: f32,
-}
-
-impl container::Stylesheet for AppStyle {
-    fn style(&self) -> Style {
-
-    }
 }
