657de5527ea78e3319cc235779ef4938fabaa607
xTeKc <bcd3v@pm.me>
2022-04-30 23:49:09 -0400

    put colors into own file

---
+++ src/colors.rs
@@ -0,0 +1,83 @@
+const PRIMARY: Color = Color {
+    r: 1.00,
+    g: 0.6,
+    b: 0.2,
+    a: 1.0,
+};
+
+const DARK: Color = Color {
+    r: 0.07,
+    g: 0.07,
+    b: 0.07,
+    a: 1.0,
+};
+
+const DARK2: Color = Color {
+    r: 0.14,
+    g: 0.14,
+    b: 0.14,
+    a: 1.0,
+};
+
+const DARK3: Color = Color {
+    r: 0.21,
+    g: 0.21,
+    b: 0.21,
+    a: 1.0,
+};
+
+const DARK4: Color = Color {
+    r: 0.28,
+    g: 0.28,
+    b: 0.28,
+    a: 1.0,
+};
+
+const LIGHT_TEXT: Color = Color {
+    r: 0.80,
+    g: 0.80,
+    b: 0.80,
+    a: 1.0,
+};
+
+const LIGHT: Color = Color {
+    r: 0.98,
+    g: 0.98,
+    b: 0.98,
+    a: 1.0,
+};
+
+const LIGHT2: Color = Color {
+    r: 0.93,
+    g: 0.93,
+    b: 0.93,
+    a: 1.0,
+};
+
+const LIGHT3: Color = Color {
+    r: 0.86,
+    g: 0.86,
+    b: 0.86,
+    a: 1.0,
+};
+
+const LIGHT4: Color = Color {
+    r: 0.79,
+    g: 0.79,
+    b: 0.79,
+    a: 1.0,
+};
+
+const DARK_TEXT: Color = Color {
+    r: 0.07,
+    g: 0.07,
+    b: 0.07,
+    a: 1.0,
+};
+
+const GRAY: Color = Color {
+    r: 0.6,
+    g: 0.6,
+    b: 0.6,
+    a: 1.0,
+};
