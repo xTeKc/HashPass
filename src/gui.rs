2d3d96cc08a3fc2853a1b2ad4b5908493fce37e9
xTeKc <bcd3v@pm.me>
2022-05-01 00:12:26 -0400

    impl textinputstyle

--- src/gui.rs
+++ src/gui.rs
@@ -38,3 +38,20 @@ pub struct TextInputStyle {
     pub is_dark_mode: bool,
 }
 
+impl Stylesheet for TextInputStyle {
+    fn active(&self) -> Style {
+        Style {
+            background: match background {
+                is_dark_mode => colors::DARK2.into(),
+                _ => colors::LIGHT.into(),
+            },
+            border_color: DARK4,
+            border_radius: 0.0,
+            border_width: match border_width {
+                is_dark_mode => { 0.0 },
+                _ => { 1.0 },
+            },
+        };
+        Style::default()
+    }
+}
