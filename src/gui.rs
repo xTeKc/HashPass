8d81d7a46c8fc1f155a7946bb7988009a324bee9
xTeKc <bcd3v@pm.me>
2022-04-30 23:40:05 -0400

    add opacity to struct

--- src/gui.rs
+++ src/gui.rs
@@ -5,8 +5,11 @@ use iced::{button, container, rule, text_input, Color};
 pub struct AppStyle {
     pub is_dark_mode: bool,
     pub is_running: bool,
+    pub opacity: f32,
 }
 
 impl container::Stylesheet for AppStyle {
-    
+    fn style(&self) -> Style {
+
+    }
 }
